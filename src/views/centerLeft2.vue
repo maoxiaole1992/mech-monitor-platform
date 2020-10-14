90012b438f035cb8cb71b2d6ea593a0afce1ba19
gyzhang <1007497297@qq.com>
2020-10-14 16:05:29 +0800

    均方根值图表

--- src/views/centerLeft2.vue
+++ src/views/centerLeft2.vue
@@ -6,7 +6,7 @@
           <icon name="chart-pie"></icon>
         </span>
         <div class="d-flex">
-          <span class="fs-xl text mx-2">数据情况雷达图</span>
+          <span class="fs-xl text mx-2">均方根值监测图</span>
           <dv-decoration-1 style="width:1.25rem;height:.25rem; position:relative;top:-.0375rem;" />
         </div>
       </div>
